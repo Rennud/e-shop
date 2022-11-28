0d2843c532ec5875f926dc0e66e3f7de21478247
Oluwagbemiga13 <daniel.rakovsky@seznam.cz>
2022-11-28 19:32:23 +0100

    Implemented GET category + changed GET List<Item>

--- e-shop-backend/src/main/java/com/example/eshopbackend/repository/ItemRepository.java
+++ e-shop-backend/src/main/java/com/example/eshopbackend/repository/ItemRepository.java
@@ -2,9 +2,17 @@ package com.example.eshopbackend.repository;
 
 import com.example.eshopbackend.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
+    List<Item> findAllByCategory(String category);
+
+    @Query(value = "SELECT * FROM item_table i WHERE i.sold = FALSE",
+            nativeQuery = true)
+    List<Item> findAllNotSold();
 }
